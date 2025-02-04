e477f329ebcf0715f2aa89b9fe4f4291a20b6c07
teuncm <teun.c.mathijssen@gmail.com>
2025-02-04 17:00:26 +0100

    Change resolution

--- main.py
+++ main.py
@@ -7,18 +7,18 @@ scale_length = 65
 # Number of frets to show.
 frets = np.arange(1, 25)
 # Number of harmonics to show nodes of.
-harmonics = [2, 3, 4, 5, 6, 7, 8, 9]
+harmonics = [2, 3, 4, 5, 6, 7]
 
 def main():
-  plt.rc('font', size=18)
+  # plt.rc('font', size=14)
 
-  _, ax = plt.subplots(figsize=(40, 2.5), layout="constrained", dpi=30)
+  _, ax = plt.subplots(figsize=(16, 2.5), layout="constrained")
 
   # Figure styling.
   ax.axhline(1, alpha=0.25, color="blue", linewidth=0.5)
   ax.axhline(0, alpha=0.25, color="blue", linewidth=0.5)
   ax.set_xlim([0, scale_length])
-  ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+  ax.xaxis.set_major_locator(ticker.MultipleLocator(5))
   ax.set_ylim([-0.5, 1.5])
   ax.set_xlabel("Distance from nut (cm)")
   ax.get_yaxis().set_visible(False)
@@ -29,7 +29,7 @@ def main():
     for numerator in range(1, harmonic):
       fraction = scale_length * numerator / harmonic
 
-      ax.plot(fraction, 0, color="black", marker="|", ms=1000, alpha=0.25, linewidth=0.5)
+      ax.plot(fraction, 0, color="black", marker="|", ms=1000, alpha=0.4, markeredgewidth=0.4)
       ax.text(fraction, 0, rf"${harmonic}f_0$", horizontalalignment="center", verticalalignment="center", bbox=bbox_style)
 
   # Mark frets with a single dot.
@@ -41,7 +41,7 @@ def main():
     # Use equal temperment for fret spacing.
     distance = scale_length - scale_length / 2 ** (fret / 12)
     
-    ax.plot(distance, 1, color="black", marker="|", ms=50, linewidth=0.5)
+    ax.plot(distance, 1, color="black", marker="|", ms=50, markeredgewidth=0.4)
     ax.text(distance, 1, f"{fret}", horizontalalignment="center", verticalalignment="center", bbox=bbox_style)
 
     if fret in marked_frets:
@@ -51,7 +51,7 @@ def main():
       ax.text(distance, 0.5, f"●●", horizontalalignment="center", verticalalignment="center")
 
   plt.savefig("flageolets.png", bbox_inches="tight", dpi=200)
-  # plt.show()
+  plt.show()
 
 if __name__ == "__main__":
   main()

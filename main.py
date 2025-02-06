5ee0b3efc3104fa8b51ec57cb1783e84dbe27bd3
teuncm <teun.c.mathijssen@gmail.com>
2025-02-06 14:53:07 +0100

    Add detune and figure title

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import numpy as np
+import math
 
 # Distance from nut to bridge in %.
 scale_length = 100
@@ -13,7 +14,7 @@ def main():
   plt.rc('font', size=10)
   plt.rc('font', family="monospace")
 
-  _, ax = plt.subplots(figsize=(18, 2.5), layout="constrained")
+  _, ax = plt.subplots(figsize=(18, 2.8), layout="constrained")
 
   # Figure styling.
   ax.axhline(1, alpha=0.25, color="blue", linewidth=0.5)
@@ -28,10 +29,13 @@ def main():
   # Reverse harmonics as multiples are drawn on top of eachother.
   for harmonic in reversed(harmonics):
     for numerator in range(1, harmonic):
-      fraction = scale_length * numerator / harmonic
+      distance = scale_length * numerator / harmonic
+      semitones = freqRatioToSemitones(harmonic)
+      cents = round(100 * (semitones - round(semitones)))
 
-      ax.plot(fraction, 0, color="black", marker="|", ms=1000, alpha=0.3, markeredgewidth=0.5)
-      ax.text(fraction, 0, rf"${harmonic}f_0$", horizontalalignment="center", verticalalignment="center", bbox=bbox_style)
+      ax.plot(distance, 0, color="black", marker="|", ms=1000, alpha=0.3, markeredgewidth=0.5)
+      ax.text(distance, 0, rf"${harmonic}f_0$", horizontalalignment="center", verticalalignment="bottom", bbox=bbox_style)
+      ax.text(distance, 0, rf"${cents:+}$", horizontalalignment="center", verticalalignment="top", bbox=bbox_style)
 
   # Mark frets with a single dot.
   marked_frets = [3, 5, 7, 9, 15, 17, 19, 21]
@@ -51,8 +55,14 @@ def main():
     if fret in marked_octaves:
       ax.text(distance, 0.5, f"●●", horizontalalignment="center", verticalalignment="center")
 
+  plt.title("Fretboard locations of first 9 flageolets with detune from nearest note")
   plt.savefig("flageolets.png", bbox_inches="tight", dpi=200)
   plt.show()
+
+def freqRatioToSemitones(freqRatio):
+  cents = 12 * math.log(freqRatio, 2)
+
+  return cents
 
 if __name__ == "__main__":
   main()
